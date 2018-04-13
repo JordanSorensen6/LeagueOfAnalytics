4ebe8eaf2bc5d5826925fb3dba5f8671ac5a24a1
Naoki Tominaga <u0876779@utah.edu>
2018-04-13 12:35:35 -0600

    Collecting Bronze data

--- ScoreCollector/ScoreCollector/Program.cs
+++ ScoreCollector/ScoreCollector/Program.cs
@@ -13,10 +13,10 @@ namespace ScoreCollector
         static void Main(string[] args)
         {
             ///////////////////////////////////////////////////////////////////////////////////////////////
-            String currentRank = "Gold";//Enter the rank you would like to gather data on. (Diamond, Platinum, Gold, Silver, Bronze) This should corrilate with the rank file. 
-            StreamReader sr = File.OpenText(@"C:\Users\Mitchell\Desktop\LOA Data\Gold.txt");//Enter the file location of the list of summoners in a particular rank. (Diamond.txt, Platinum.txt, Gold.txt, Silver.txt, Bronze.txt)
-            StreamWriter scoreLog = File.CreateText(@"C:\Users\Mitchell\Desktop\LOGGOLD01.txt");//Enter the file location for log file. You can call the file whatever you want. Make sure you change the name after each run to avoid overwriting data.
-            String startingSummoner = "make me win";//Enter a summoner from the list of summoners to start collecting data or continue from last point.
+            String currentRank = "Bronze";//Enter the rank you would like to gather data on. (Diamond, Platinum, Gold, Silver, Bronze) This should corrilate with the rank file. 
+            StreamReader sr = File.OpenText(@"/Users/Naoki/Dropbox/Senior Project/LeagueOfAnalytics/LOA Data/Bronze.txt");//Enter the file location of the list of summoners in a particular rank. (Diamond.txt, Platinum.txt, Gold.txt, Silver.txt, Bronze.txt)
+            StreamWriter scoreLog = File.CreateText(@"/Users/Naoki/Dropbox/Senior Project/LeagueOfAnalytics/LOA Data/LOGBRONZE01.txt");//Enter the file location for log file. You can call the file whatever you want. Make sure you change the name after each run to avoid overwriting data.
+            String startingSummoner = "ASC Robin";//Enter a summoner from the list of summoners to start collecting data or continue from last point.
             ///////////////////////////////////////////////////////////////////////////////////////////////
 
 
