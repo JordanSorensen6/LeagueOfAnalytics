c25fa0e89b3f671ccb8088ba739cc9a014a2aa90
Naoki Tominaga <u0876779@utah.edu>
2018-04-24 14:47:34 -0600

    commit b4 checkout

--- src/main/resources/javascript/BarChart.js
+++ src/main/resources/javascript/BarChart.js
@@ -1,258 +1,39 @@
 class BarChart{
 
-    /**
-     * Load the file indicated by the select menu
-     */
-    function changeData() {
-        let dataFile = document.getElementById('dataset').value;
-        if (document.getElementById('random').checked) {
-            randomSubset();
-        }
-        else {
-            d3.csv('data/' + dataFile + '.csv', update);
-        }
+
+    constructor(data){
+        this.data = data;
     }
 
-    /**
-     * Render the visualizations
-     * @param error
-     * @param data
-     */
-    function update(error, data) {
-        if (error !== null) {
-            alert('Could not load the dataset!');
-        } else {
-            // D3 loads all CSV data as strings;
-            // while Javascript is pretty smart
-            // about interpreting strings as
-            // numbers when you do things like
-            // multiplication, it will still
-            // treat them as strings where it makes
-            // sense (e.g. adding strings will
-            // concatenate them, not add the values
-            // together, or comparing strings
-            // will do string comparison, not
-            // numeric comparison).
+    updateChart(){
+        let padding = 60;
+        let width = 500 - 2 * padding;
+        let height = 400 - 2 * padding;
 
-            // We need to explicitly convert values
-            // to numbers so that comparisons work
-            // when we call d3.max()
+        let maxScore = d3.max(this.data, function (d){
+            return d["score"];
+        });
+        let minScore = d3.min(this.data, function (d){
+            return d["score"];
+        });
 
-            for (let d of data) {
-                d.a = +d.a;
-                d.b = +d.b;
-            }
-        }
+        let maxPercentage = d3.max(this.data, function (d){
+            return d["percentage"];
+        });
+        let minPercentage = d3.min(this.data, function (d){
+            return d["percentage"];
+        });
 
-        // Set up the scales
-        let aScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.a)])
-    .range([0, 150]);
-        let bScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.b)])
-    .range([0, 150]);
-        let iScale = d3.scaleLinear()
-            .domain([0, data.length])
-            .range([0, 110]);
+        let xScale = d3.scaleLinear()
+            .domain([minScore, maxScore])
+            .range([0, width]);
+        let xAxis = d3.axisBottom();
+        xAxis.scale(xScale);
+        d3.select('#xAxis')
+            .classed("axis", true)
+            .attr("transform", "translate(" + padding + "," + (height + padding) + ")")
+            .call(xAxis);
 
-
-        // ****** TODO: PART III (you will also edit in PART V) ******
-
-        // TODO: Select and update the 'a' bar chart bars
-        var barChart1 = d3.select("#barChart1").selectAll("rect")
-            .data(data);
-
-
-        barChart1.exit()
-            .attr("opacity", 1)
-            .transition()
-            .duration(3000)
-            .attr("opacity", 0)
-            .remove();
-
-        barChart1 = barChart1.enter().append("rect")
-            .merge(barChart1);
-
-        barChart1
-            .transition()
-            .duration(3000)
-            .attr("height", function(d, i){
-                return aScale(d.a);
-            })
-            .attr("width", 10)
-            .attr("x",function(d, i){
-                return iScale(i);
-            })
-            .attr("y", 0)
-            .attr("fill", "steelblue");
-
-        // TODO: Select and update the 'b' bar chart bars
-        var barChart2 = d3.select("#barChart2").selectAll("rect")
-            .data(data)
-            .attr("height", function(d, i){
-                return bScale(d.b);
-            })
-            .attr("x",function(d, i){
-                return iScale(i);
-            });
-
-        barChart2.exit()
-            .attr("opacity", 1)
-            .transition()
-            .duration(3000)
-            .attr("opacity", 0)
-            .remove();
-
-        barChart2 = barChart2.enter().append("rect")
-            .merge(barChart2);
-
-        barChart2
-            .transition()
-            .duration(3000)
-            .attr("height", function(d, i){
-                return bScale(d.b);
-            })
-            .attr("width", 10)
-            .attr("x",function(d, i){
-                return iScale(i);
-            })
-            .attr("y", 0)
-            .attr("fill", "steelblue");
-
-        // TODO: Select and update the 'a' line chart path using this line generator
-
-        let aLineGenerator = d3.line()
-            .x((d, i) => iScale(i))
-    .y((d) => aScale(d.a));
-
-        var lineChart1 = d3.select("#line1")
-            .transition()
-            .duration(3000)
-            .attr("d", aLineGenerator(data));
-
-        // TODO: Select and update the 'b' line chart path (create your own generator)
-        let bLineGenerator = d3.line()
-            .x((d, i) => iScale(i))
-    .y((d) => bScale(d.b));
-
-        var lineChart2 = d3.select("#line2")
-            .transition()
-            .duration(3000)
-            .attr("d", bLineGenerator(data));
-
-        // TODO: Select and update the 'a' area chart path using this area generator
-        let aAreaGenerator = d3.area()
-            .x((d, i) => iScale(i))
-    .y0(0)
-            .y1(d => aScale(d.a));
-
-        var areaChart1 = d3.select("#area1")
-            .transition()
-            .duration(3000)
-            .attr("d", aAreaGenerator(data));
-
-        // TODO: Select and update the 'b' area chart path (create your own generator)
-        let bAreaGenerator = d3.area()
-            .x((d, i) => iScale(i))
-    .y0(0)
-            .y1(d => bScale(d.b));
-
-        var areaChart2 = d3.select("#area2")
-            .transition()
-            .duration(3000)
-            .attr("d", bAreaGenerator(data));
-
-        // TODO: Select and update the scatterplot points
-        var scatter = d3.select("#scatter").selectAll("circle")
-            .data(data);
-
-
-        scatter.exit()
-            .attr("opacity", 1)
-            .transition()
-            .duration(3000)
-            .attr("opacity", 0)
-            .remove();
-
-        scatter = scatter.enter().append("circle")
-            .merge(scatter);
-
-        scatter
-            .transition()
-            .duration(3000)
-            .attr("cx", function(d, i){
-                return aScale(d.a);
-            })
-            .attr("cy", function(d, i){
-                return bScale(d.b);
-            })
-            .attr("r", 5)
-            .style("fill", "steelblue");
-
-        // ****** TODO: PART IV ******
-
-        d3.selectAll("rect")
-            .on("mouseover", function(){
-                d3.select(this)
-                    .attr("fill", "orange");
-            })
-            .on("mouseout", function(d, i) {
-                d3.select(this)
-                    .attr("fill", "steelblue");
-            });
-
-        var tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .attr("opacity", 0);
-        d3.selectAll("circle")
-            .on("mouseover", function(){
-
-                tooltip.transition()
-                    .duration(300)
-                    .attr("border", "0px")
-                    .attr("opacity", .9);
-                tooltip.html("x: " + d3.select(this).attr("cx") + " " + "y: " + d3.select(this).attr("cy"));
-            })
-            .on("mouseout", function(){
-                tooltip.transition()
-                    .duration(400)
-                    .attr("opacity", 0);
-            });
-
+        //let yScale =
     }
-    // constructor(data){
-    //     this.data = data;
-    // }
-    //
-    // updateChart(){
-    //     let padding = 60;
-    //     let width = 500 - 2 * padding;
-    //     let height = 400 - 2 * padding;
-    //
-    //     let maxScore = d3.max(this.data, function (d){
-    //         return d["score"];
-    //     });
-    //     let minScore = d3.min(this.data, function (d){
-    //         return d["score"];
-    //     });
-    //
-    //     let maxPercentage = d3.max(this.data, function (d){
-    //         return d["percentage"];
-    //     });
-    //     let minPercentage = d3.min(this.data, function (d){
-    //         return d["percentage"];
-    //     });
-    //
-    //     let xScale = d3.scaleLinear()
-    //         .domain([minScore, maxScore])
-    //         .range([0, width]);
-    //     let xAxis = d3.axisBottom();
-    //     xAxis.scale(xScale);
-    //     d3.select('#xAxis')
-    //         .classed("axis", true)
-    //         .attr("transform", "translate(" + padding + "," + (height + padding) + ")")
-    //         .call(xAxis);
-    //
-    //     //let yScale =
-    // }
 }
--- src/main/webapp/web/appstats.jsp
+++ src/main/webapp/web/appstats.jsp
@@ -9,73 +9,30 @@
     <script src="/resources/javascript/bootstrap/bootstrap.min.js"></script>
 
     <script src="https://d3js.org/d3.v4.js"></script>
-    <script type="application/javascript" src="/resources/javascript/BarChart.js"></script>
+    <script src="/resources/javascript/BarChart.js"></script>
 </head>
 <body>
 
 <jsp:include page="navbar.jsp"/>
 
-<span>
-    <label>Dataset:</label>
-    <select id="dataset" onchange="changeData()">
-        <option value="anscombe_I">Anscombe's Quartet I</option>
-        <option selected value="anscombe_II">Anscombe's Quartet II</option>
-        <option value="anscombe_III">Anscombe's Quartet III</option>
-        <option value="anscombe_IV">Anscombe's Quartet IV</option>
-    </select>
-</span>
 
-<div>
-    <h2>Bar Charts</h2>
-    <svg width="200" height="200">
-        <g id = "barChart1" transform="translate(0, 200) scale(1, -1)" class="barChart" >
-            <rect x="10" y="0" width="10" height="100"></rect>
-            <rect x="20" y="0" width="10" height="80"></rect>
-            <rect x="30" y="0" width="10" height="130"></rect>
-            <rect x="40" y="0" width="10" height="90"></rect>
-            <rect x="50" y="0" width="10" height="110"></rect>
-            <rect x="60" y="0" width="10" height="140"></rect>
-            <rect x="70" y="0" width="10" height="60"></rect>
-            <rect x="80" y="0" width="10" height="40"></rect>
-            <rect x="90" y="0" width="10" height="120"></rect>
-            <rect x="100" y="0" width="10" height="70"></rect>
-            <rect x="110" y="0" width="10" height="50"></rect>
-        </g>
-    </svg>
-
-    <svg width="200" height="200">
-        <g id = "barChart2" transform="translate(0, 200) scale(1, -1)" class="barChart" >
-            <rect x="10" y="0" width="10" height="91"></rect>
-            <rect x="20" y="0" width="10" height="81"></rect>
-            <rect x="30" y="0" width="10" height="87"></rect>
-            <rect x="40" y="0" width="10" height="88"></rect>
-            <rect x="50" y="0" width="10" height="93"></rect>
-            <rect x="60" y="0" width="10" height="81"></rect>
-            <rect x="70" y="0" width="10" height="61"></rect>
-            <rect x="80" y="0" width="10" height="31"></rect>
-            <rect x="90" y="0" width="10" height="91"></rect>
-            <rect x="100" y="0" width="10" height="73"></rect>
-            <rect x="110" y="0" width="10" height="47"></rect>
-        </g>
-    </svg>
-</div>
 
 </body>
 
 <script>
     $('#nav-app').addClass('active');
 
-    // d3.json("/resources/data/gold_data.json", function (error, data) {
-    //
-    //     data.forEach(function (d) {
-    //         d["total games"] = d.wins + d.losses;
-    //         d["percentage"] = d.wins/d["total games"]*100;
-    //     });
-    //
-    //     let chart = new BarChart(data);
-    //     chart.updateChart();
-    //
-    // });
+    d3.json("/resources/data/gold_data.json", function (error, data) {
+
+        data.forEach(function (d) {
+            d["total games"] = d.wins + d.losses;
+            d["percentage"] = d.wins/d["total games"]*100;
+        });
+
+        let chart = new BarChart(data);
+        chart.updateChart();
+
+    });
 
 
 </script>
