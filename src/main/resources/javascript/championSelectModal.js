30608ecb94b035257b6fab75fc094617d519706b
Jordan Sorensen <jordan.sorensen6@gmail.com>
2018-10-17 11:53:00 -0600

    some champion select fixes

--- src/main/resources/javascript/championSelectModal.js
+++ src/main/resources/javascript/championSelectModal.js
@@ -1,6 +1,7 @@
 var championSelectModal = (function($) {
 
     var selectFor;
+    var currentDataFilter = '*';
 
     function init(selectedFunction) {
         getChampionsWithTags();
@@ -31,12 +32,14 @@ var championSelectModal = (function($) {
             $('.filter-options .current-filter').removeClass('current-filter');
             $(this).addClass('current-filter');
             var selector = $(this).attr('data-filter');
+            currentDataFilter = selector;
             if(selector === '*')
                 $('#championImages > div').show();
             else {
                 var $el = $('.' + selector).show();
                 $('#championImages > div').not($el).hide();
             }
+            $('#championSearch').trigger('keyup');
         });
     }
 
@@ -47,10 +50,15 @@ var championSelectModal = (function($) {
     function setupSearch() {
         $('#championSearch').on('keyup', function() {
             var value = formatChampionName($(this).val());
-            $('#championImages div').filter(function() {
-                if(!$(this).is(':hidden'))
+            if(currentDataFilter !== '*') {
+                $('#championImages div.' + currentDataFilter).filter(function () {
                     $(this).toggle($(this).attr('id').indexOf(value) > -1)
-            });
+                });
+            } else {
+                $('#championImages div').filter(function () {
+                    $(this).toggle($(this).attr('id').indexOf(value) > -1)
+                });
+            }
         });
     }
 
@@ -59,7 +67,10 @@ var championSelectModal = (function($) {
         var noImages = ['kaisa', 'kayn', 'ornn', 'pyke', 'rakan', 'xayah', 'zoe', 'wukong'];
         for(var name in champions) {
             if(!(noImages.indexOf(name) > -1))
-                championContainer.append('<div id="' + name + '" class="champ-img ' + champions[name] + '"><img src="/resources/images/champion/' + name + '.png"></div>');
+                championContainer.append('' +
+                    '<div id="' + name + '" class="champ-img ' + champions[name].toLowerCase() + '">' +
+                    '<img src="/resources/images/champion/' + name + '.png">' +
+                    '</div>');
         }
 
         $('.champ-img').on('click', function() {
